5768bd544af3bcd026c3f9e4d6d957afbf5dbf1c
shraddhabasnyat <basnyatshraddha@gmail.com>
2026-04-03 13:51:36 +0545

    add no bufferring for SSE

--- backend/src/_lib/stream.ts
+++ backend/src/_lib/stream.ts
@@ -9,6 +9,7 @@ export function createSSEStream(res: Response): SSEStream {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
+  res.setHeader("X-Accel-Buffering", "no");
   res.flushHeaders();
 
   function emit(eventName: string, data: object): void {
